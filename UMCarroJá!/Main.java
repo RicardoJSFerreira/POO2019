138341cb73d7442ceaed268ac3f2bbd017f75552
Gabriela  Martins <gabi.martins1909@gmail.com>
2019-04-17 02:31:43 +0100

    Testes

--- UMCarroJá!/Main.java
+++ UMCarroJá!/Main.java
@@ -2,10 +2,29 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 
-public class Main implements Serializable {
+public class Main extends Cliente implements Serializable {
 
-    public static void main() throws FileNotFoundException, IOException,ClassNotFoundException{}
-
-
+    public static void main() {
+        
+    	Cliente cli = new Cliente(1, "rjsf311@gmail.com", "Ricardo", "12345", "Rua das Flores", "15-08-1998", 2.0, 3.0, 10);
+        System.out.println(cli);
+        
+        Veiculo v =  new Veiculo(1234,true,66.9,1.3,5.6,10,20,30,4,6);
+        System.out.println(v);
+        
+        ArrayList<Veiculo> veiculoList = new ArrayList<Veiculo>();
+        veiculoList.add(v);
+        Proprietario p = new Proprietario(23,"pp@gmail.com","miguel","pass","rua das bolinhas","21/07/1998",2000,veiculoList);
+        System.out.println(p);
+        
+        List<User> listaUsers = new ArrayList<>();
+        listaUsers.add(cli);
+        listaUsers.add(p);
+        System.out.println(listaUsers);
+        
+        List<Veiculo> listaVeiculos = new ArrayList<>();
+        listaVeiculos.add(v);
+        System.out.println(listaVeiculos);
+    }
 
 }
