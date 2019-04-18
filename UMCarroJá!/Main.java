26453571748dcd1cc167adda9b69616fc0742b5f
Gabriela  Martins <gabi.martins1909@gmail.com>
2019-04-18 01:54:34 +0100

    main

--- UMCarroJá!/Main.java
+++ UMCarroJá!/Main.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main extends Cliente implements Serializable {
+public class Main implements Serializable {
 
     public static void main() {
         
